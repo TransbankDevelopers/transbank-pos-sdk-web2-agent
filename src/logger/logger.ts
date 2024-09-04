9bb6796cedc4060f2f7eee0eafacec864ef6e54c
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-09-04 16:21:06 -0400

    feat: add logger

---
+++ src/logger/logger.ts
@@ -0,0 +1,27 @@
+import { app } from "electron";
+import path from "path";
+import winston from "winston";
+import "winston-daily-rotate-file";
+
+const logDirectory = app.getPath("logs");
+
+const dailyRotateFileTransport = new winston.transports.DailyRotateFile({
+  filename: path.join(logDirectory, "%DATE%.log"),
+  datePattern: "DD-MM-YYYY",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "30d",
+});
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "HH:mm:ss" }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [new winston.transports.Console(), dailyRotateFileTransport],
+});
+
+export default logger;
